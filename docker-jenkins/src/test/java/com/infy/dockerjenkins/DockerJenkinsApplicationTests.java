0382e80b8ce1c4cdc5c8f2b59d39e4f603801840
Rugved <Rugved@jenkins-server.te5gypnj4c3udczeduk3cx0oie.px.internal.cloudapp.net>
2023-05-01 07:06:46 +0000

    first commit

---
+++ docker-jenkins/src/test/java/com/infy/dockerjenkins/DockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.dockerjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
